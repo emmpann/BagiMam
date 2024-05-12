afac86c9837e8c83d482aff8830a52ac9b2ab03f
wahyunugrahha <wahyunih297@gmail.com>
2024-05-12 15:30:08 +0700

    add profile fragment

--- app/src/main/java/com/github/emmpann/bagimam/home/HomeFragment.kt
+++ app/src/main/java/com/github/emmpann/bagimam/home/HomeFragment.kt
@@ -1,21 +1,39 @@
 package com.github.emmpann.bagimam.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.github.emmpann.bagimam.R
+import com.github.emmpann.bagimam.databinding.FragmentHomeBinding
+
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Set click listener for iv_Profile if it's found
+        binding.ivProfile?.setOnClickListener {
+            // Navigate to ProfileFragment
+            findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
