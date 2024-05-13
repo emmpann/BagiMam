857f339d65dff737be631b68d63650254fe4d6ca
wahyunugrahha <wahyunih297@gmail.com>
2024-05-13 23:43:17 +0700

    minor update

--- core/src/main/java/com/github/emmpann/core/domain/model/Food.kt
+++ core/src/main/java/com/github/emmpann/core/domain/model/Food.kt
@@ -3,5 +3,6 @@ package com.github.emmpann.core.domain.model
 data class Food(
     val name: String = "",
     val type: String = "",
-    val amount: String = ""
+    val amount: String = "",
+    val address: String = ""
 )
